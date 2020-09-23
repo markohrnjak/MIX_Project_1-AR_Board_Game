a20763079670889c88d6db050f0634f5e3f40c2b
markohrnjak <markohrnjak2@gmail.com>
2020-09-23 19:16:24 +0200

    .

---
+++ Assets/Scripts/LightBulbOn.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LightBulbOn : MonoBehaviour
+{
+
+    private string message;
+    private float displayTime;
+    private bool displayMessage = false;
+
+    // Start is called before the first frame update
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "gate")
+        {
+            Debug.Log("Gate crossed");
+
+            message = "ON";
+            displayMessage = true;
+            displayTime = 6f;
+        }
+    }
+    void Update()
+    {
+        displayTime -= Time.deltaTime;
+        if (displayTime <= 0.0)
+        {
+            displayMessage = false;
+        }
+    }
+
+    void OnGUI()
+    {
+        if (displayMessage)
+        {
+            GUI.Label(new Rect(10, 10, 150, 20), message);
+        }
+    }
+
+}

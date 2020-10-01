682d3bc8d58c1b3e7cba5c4bb27cfab1d53d0c1a
markohrnjak <markohrnjak2@gmail.com>
2020-10-01 16:55:22 +0200

    import Matej Branch + rescaling

---
+++ Assets/Scripts/BoardTile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//adds a box collider if the object does not have one
+[RequireComponent(typeof(BoxCollider))]
+public class BoardTile : MonoBehaviour
+{
+    public Color onCollisionColor;
+    private BoxCollider coll;
+
+    private void Awake()
+    {
+        coll = this.GetComponent<BoxCollider>();
+        coll.isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Put code here to execute when other is collided with
+        this.GetComponent<Renderer>().material.color = onCollisionColor;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //Put code here to execute when other no longer collided with
+        this.GetComponent<Renderer>().material.color = Color.white;
+    }
+}

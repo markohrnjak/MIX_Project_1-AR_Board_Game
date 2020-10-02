dede62cfaab9c29579c8d4a821076dd2c3936262
markohrnjak <markohrnjak2@gmail.com>
2020-10-02 15:39:46 +0200

    Interaction
    Edits made to collider script and image target size.
    Monkey King bugged so removed. (floats around on detection? something with controller I htik)
    Added animation on trigger enter

--- Assets/Scripts/BoardTile.cs
+++ Assets/Scripts/BoardTile.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+//this script is attached to the tiles of the board, its purpose is to trigger effect on gameobjects that "collide with a tile".
+
 //adds a box collider if the object does not have one
 [RequireComponent(typeof(BoxCollider))]
 public class BoardTile : MonoBehaviour
@@ -18,11 +21,18 @@ public class BoardTile : MonoBehaviour
     {
         //Put code here to execute when other is collided with
         this.GetComponent<Renderer>().material.color = onCollisionColor;
+
+        Debug.Log("Space enter");
+        other.gameObject.GetComponent<Animator>().SetBool("Hit", true); //play "hit" animation
     }
 
     private void OnTriggerExit(Collider other)
     {
         //Put code here to execute when other no longer collided with
         this.GetComponent<Renderer>().material.color = Color.white;
+
+        Debug.Log("Space exit");
+        other.gameObject.GetComponent<Animator>().SetBool("Hit", false); //stop "hit" animation
+
     }
 }
